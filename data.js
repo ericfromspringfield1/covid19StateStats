b82d0b989ce7b55911d9da31f5fc18e975405be8
Eric Taylor <ericfromspringfield@gmail.com>
2020-05-19 12:57:52 -0500

    DRYer Code with styling

--- data.js
+++ data.js
@@ -25,55 +25,68 @@ fetch(dataURL)
   
   
 
-  /*document.getElementById("stateData").innerHTML = `${data.state}`
-  document.getElementById("positiveData").innerHTML = `${stateObj.positive}`
+  /*document.getElementById("positiveData").innerHTML = `${stateObj.positive}`
   document.getElementById("negativeData").innerHTML = `${stateObj.negative}`
   document.getElementById("totalTestResultsData").innerHTML = `${stateObj.totalTestResults}`
   document.getElementById("deathData").innerHTML = `${stateObj.death}`
   document.getElementById("percentPositive").innerHTML = percentPositive
   document.getElementById("percentDeathOfPositive").innerHTML = percentDeathOfPositive*/
-
   
-
+  
+  
   //Loop will let us write one line and have conditional styling throughout table 
   
-
   
-  const stateWrapper = document.querySelector("div");
+  
+  const stateWrapper = document.querySelector("table");
   const renderToDom = resultsString => {
     stateWrapper.innerHTML += resultsString;
     console.log(stateWrapper)
   };
+  
   const resultsHtmlRepresentation = stateObj => {
+    let percentPositive = (stateObj.positive / stateObj.totalTestResults * 100).toFixed(2)
+    let percentDeathOfPositive = (stateObj.death / stateObj.positive * 100).toFixed(2)
+    /*let redPercentPositive = document.getElementById("percentPositiveRow").innerHTML 
+    let whitePercentPositive = document.getElementById("percentPositiveRow").innerHTML
+    
+    if (percentPositive >= 10) {
+      redPercentPositive.style.backgroundColor = "red";
+      whitePercentPositive.style.color = "white";
+    }*/
+    
+    
+    //This just returns the final object in the array rather than the actual max death rate state
+    /*let maxDeath = Math.max(percentDeathOfPositive)
+    document.getElementById("writtenP1").innerHTML = `The highest death rate percentage is ${maxDeath}% in ${stateObj.state}`*/
+    
+    
     
     return `
     <body>
-    <div class="wrapper">
+    <div class=${"wrapper"}>
     <table rowspan=${data.length} style="width:100%">
-    
+    <p id=${"writtenP1"}></p>
     <tr>
-    <td id="stateData">${stateObj.state}</td>
-    <td id="positiveData">${stateObj.positive}</td>
-    <td id="negativeData">${stateObj.negative}</td>
-    <td id="totalTestResultsData">${stateObj.totalTestResults}</td>
-    <td id="deathData">${stateObj.death}</td>
-    <td id="percentPositive">${stateObj.positive / stateObj.totalTestResults * 100}</td>
-    <td id="percentDeathOfPositive">${stateObj.death / stateObj.positive * 100}</td>
+    <td>${stateObj.state}</td>
+    <td>${stateObj.positive}</td>
+    <td>${stateObj.negative}</td>
+    <td>${stateObj.totalTestResults}</td>
+    <td>${stateObj.death}</td>
+    <td id=${"percentPositiveRow"}>${percentPositive}</td>
+    <td>${percentDeathOfPositive}</td>
     </tr>
     </table>
+    </div>
     `;
   };
-    if (percentPositive > 10){
-      document.getElementById("percentPositive").style.backgroundColor = "red";
-      document.getElementById("percentPositive").style.color = "white";
-    }
-    
-    data.forEach(stateObj => {
-        const HtmlString = resultsHtmlRepresentation(stateObj);
-        renderToDom(HtmlString);
-    
-      }) 
-    })
   
   
+  data.forEach(stateObj => {
+    const HtmlString = resultsHtmlRepresentation(stateObj);
+    renderToDom(HtmlString); 
+  })
+})
+
+
 
